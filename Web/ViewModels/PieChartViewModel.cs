6d14bfce4800b6fef19fddf7b35fe05bf89d17c8
Kevin van Dijk <kevinvd05@gmail.com>
2025-08-23 00:16:17 +0200

    Made pie chart widget customizable

---
+++ Web/ViewModels/PieChartViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.ViewModels;
+
+public class PieChartViewModel
+{
+    public string Title { get; set; } = string.Empty;
+    public List<PieChartData> Data { get; set; } = new();
+}
+
+public class PieChartData
+{
+    public string Label { get; set; } = string.Empty;
+    public decimal Value { get; set; } = 0;
+}
