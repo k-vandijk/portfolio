bd69160254bccfddb5050a18184b6be2e120f74f
Kevin <kevinvd05@gmail.com>
2026-01-31 19:51:11 +0100

    Refactor dashboard mode handling with constants (#81)

---
+++ src/Dashboard.Domain/Utils/DashboardPresentationModes.cs
@@ -0,0 +1,8 @@
+﻿namespace Dashboard.Domain.Utils;
+
+public static class DashboardPresentationModes
+{
+    public const string Value = "value";
+    public const string Profit = "profit";
+    public const string ProfitPercentage = "profit-percentage";
+}
